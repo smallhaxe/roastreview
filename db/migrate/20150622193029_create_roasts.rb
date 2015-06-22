7ac15fdab1e8b35f627d04c49a82fbac747866f7
Ugo Aniukwu <ugo@smallhaxe.com>
2015-06-22 16:47:24 -0700

    changed migration for roasts: color to level

--- db/migrate/20150622193029_create_roasts.rb
+++ db/migrate/20150622193029_create_roasts.rb
@@ -1,7 +1,7 @@
 class CreateRoasts < ActiveRecord::Migration
   def change
     create_table :roasts do |t|
-      t.string :color
+      t.string :level
       t.string :type
       t.string :coffee_name
       t.string :roaster
