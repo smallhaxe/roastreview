7442b5eeeeb6579b83fd0f422e9ce06361713b7a
Celeste Layne <layne.celeste@gmail.com>
2015-06-22 22:52:10 -0700

    sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.confirm(params.require(:user).permit(:email, :password))
+    if @user
+      login(@user)
+      flash[:success] = "Successful login"
+      redirect_to user_path
+    else
+      flash[:error] = "Invalid email address or password. Please try again."
+      redirect_to login_path
+    end
+
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
+
+
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user.try(:authenticate, params[:password])
+  end
+
+
 end
