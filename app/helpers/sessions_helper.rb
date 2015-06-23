f618724a960270de185f12ceff1c72dfbaad4bc7
Ugo Aniukwu <ugo@smallhaxe.com>
2015-06-23 16:49:29 -0700

    incomplete

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,23 @@
 module SessionsHelper
+  
+  def current_user
+    return false if session[:user_id] == nil
+    @current_user ||= User.find(session[:user_id])
+  end
+  
+  # def logged_in?
+  #   unless current_user
+  #     redirect_to ("/")
+  #   end
+  # end
 
   def login(user)
     session[:user_id] = user.id
     @current_user = user
   end
-
-  def current_user
-    return false if session[:user_id] == nil
-    @current_user ||= User.find(session[:user_id])
+  
+  def logout
+    @current_user = session[:user_id] = nil
   end
+
 end
