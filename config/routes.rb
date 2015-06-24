55bd76989a228328d74bb8a1ec6eea3785b35bf0
Ugo Aniukwu <smallhaxe@users.noreply.github.com>
2015-06-24 13:12:32 -0700

    removing unrelated changes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
 
   post "/sessions", to: "sessions#create"
 
-  get "/sessions", to: "sessions#destroy"
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
