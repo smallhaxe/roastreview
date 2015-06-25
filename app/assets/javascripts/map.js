325b7184945ba7c8a1ab1c10b6621bee4fb106cd
Celeste Layne <layne.celeste@gmail.com>
2015-06-25 16:58:37 -0700

    mapbox tooltips finally working for real tho

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -7,36 +7,6 @@ $(document).on("ready", function(){
   var geojson;
   var marker;
   var properties;
-
-  // myMap.map.addLayer(myMap.cafeLayer);
-  // myMap.map.featureLayer.on("ready", function(e) {
-  //   myMap.getCafes(myMap.map);
-  // });
-
-  // customize the marker events
-  myMap.addCafePopups = function(map) {
-//
-    map.featureLayer.on("layeradd", function(e){
-    marker = e.layer;
-    properties = marker.feature.properties;
-    popupContent = '<div class="leaflet-popup">' + '<h3>' + properties.name + '</h3>' +
-                    '<h4>' + properties.address + '</h4>' + '</div>';
-
-  myMap.cafeLayer = L.mapbox.featureLayer().bindPopup(popupContent + "foobar");
-});
-      // console.log(myMap.cafeLayer['popupContent'])
-      // myMap.cafeLayer = L.mapbox.featureLayer().bindPopup('hi');
-
-
-      // marker.bindPopup(popupContent, {
-      //   closeButton: false,
-      //   minWidth: 300});
-      // marker.eachLayer(function(e){
-      //   e.openPopup();
-      // });
-    //}).addTo(myMap.map);
-  }
-
   var popupContent;
 
   myMap.getCafes = function(map) {
@@ -50,47 +20,35 @@ $(document).on("ready", function(){
         geojson = $.parseJSON(cafes);
         // load the markers in the map
 
-
         myMap.cafeLayer.setGeoJSON({
           type: "FeatureCollection",
           features: geojson
         });
         // popup behaviour on marker mouseover event
-        // myMap.cafeLayer.on('mouseover', function(e) {
           myMap.cafeLayer.on("mouseover", function(e){
-          marker = e.layer;
-          properties = marker.feature.properties;
-          popupContent = '<div class="leaflet-popup">' + '<h3>' + properties.name + '</h3>' +
-                          '<h4>' + properties.address + '</h4>' + '</div>';
-          marker.bindPopup(popupContent, {
-            closeButton: false,
-            minWidth: 320,
-            minHeight: 200
+            marker = e.layer;
+            properties = marker.feature.properties;
+            popupContent = '<div class="leaflet-popup">' + '<h3>' + properties.name + '</h3>' +
+                            '<h4>' + properties.address + '</h4>' + '</div>';
+            marker.bindPopup(popupContent, {
+              closeButton: false,
+              minWidth: 320,
+              minHeight: 200
             //styles in here
-          })
-          // marker = e.layer;
-          // properties = marker.feature.properties;
-          // console.log(marker);
-          // console.log(properties);
-          // popupContent = '<div class="leaflet-popup">' + '<h3>' + properties.name + '</h3>' +
-          //                 '<h4>' + properties.address + '</h4>' + '</div>';
-          // console.log(popupContent);
-            this.openPopup();
+          });
+          marker.openPopup();
         });
 
         myMap.cafeLayer.on('mouseout', function(e) {
-            e.layer.closePopup();
+            marker.closePopup();
         });
-        // .bindPopup(popupContent);
-        // myMap.addCafePopups(map);
       },
-
-      // error:function(){
-      //   alert("Could not load the cafes");
-      // }
+      error:function(){
+        alert("Could not load the cafes");
+      }
     });
   }
-  myMap.cafeLayer = L.mapbox.featureLayer().bindPopup(popupContent);
+  myMap.cafeLayer = L.mapbox.featureLayer();
   myMap.map.addLayer(myMap.cafeLayer);
   myMap.map.featureLayer.on("ready", function(e) {
     myMap.getCafes(myMap.map);
