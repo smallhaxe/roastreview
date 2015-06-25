b3a3d8bf463440940af18b4beaa2c0ca7a140ac3
wyattsweet <wyattsweet@gmail.com>
2015-06-25 11:04:30 -0700

    edit profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,12 +43,12 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, alert: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+      if @current_user.update(user_params)
+        format.html { redirect_to @current_user, alert: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @current_user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @current_user.errors, status: :unprocessable_entity }
       end
     end
   end
