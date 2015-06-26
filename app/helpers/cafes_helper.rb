c9d605dee8825ef9cc152147162a98b39494da67
Celeste Layne <layne.celeste@gmail.com>
2015-06-26 00:00:45 -0700

    add link to cafe cards

--- app/helpers/cafes_helper.rb
+++ app/helpers/cafes_helper.rb
@@ -1,8 +1,8 @@
 module CafesHelper
 
-  def current_cafe
-    cafe_properties = @cafesjson.select { |cafe_name| cafe_name.even? }
-    @current_cafe ||= Cafe.find_by(id: cafe_marker[:id])
-  end
+  # def current_cafe
+  #   cafe_properties = @cafesjson.select { |cafe_obj| cafe_obj.properties.name == ? }
+  #   @current_cafe ||= Cafe.find_by(id: cafe_marker[:id])
+  # end
 
 end
